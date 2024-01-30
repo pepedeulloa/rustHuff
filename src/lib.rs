7930ff0207dda5c0824a4298bc565610d9a77908
pepedeulloa <lucascounhagobarreiro@gmail.com>
2024-01-30 01:08:28 +0100

    Created Huffman Binary Tree and Nodes. Implemented some methods for this types

--- src/lib.rs
+++ src/lib.rs
@@ -18,10 +18,87 @@ pub mod cli {
  pub fn open_file (filename: PathBuf) -> Result<BufReader<File>,Box<dyn Error>> {
   let file = File::open(filename)?;
   let reader: BufReader<File>;
-   
+
   reader = BufReader::new(file);
   Ok(reader)
  }
+}
+
+pub mod huff_btree {
+    use std::io::Error;
+
+	#[derive(Debug)]
+	enum HuffNode{
+	Branch {
+		weight: usize,
+		left: Box<HuffNode>,
+		right: Box<HuffNode>,
+	},
+	Leaf {
+		char: char,
+		weight: usize
+	}
+}
+
+impl HuffNode {
+
+	fn get_weight(&self) -> usize {
+		match *self {
+			HuffNode::Branch { weight, .. }=> return weight,
+			HuffNode::Leaf { weight, .. }=> return weight,
+		}
+	}
+
+	fn create_branch(left: Box<HuffNode>, right: Box<HuffNode>) -> HuffNode {
+		let weight = left.get_weight() + right.get_weight();
+		HuffNode::Branch { weight, left: Box::new(*left), right: Box::new(*right) }
+	}
+
+	fn create_leaf(char: char, weight: usize) -> HuffNode {
+		HuffNode::Leaf { char, weight }
+	}
+
+}
+
+	struct HuffBTree {
+		root: Option<Box<HuffNode>>
+	}
+
+	impl HuffBTree {
+		fn new(min: (char,usize), max: (char,usize)) -> HuffBTree {
+
+			let left = HuffNode::create_leaf(min.0, min.1);
+			let right = HuffNode::create_leaf(max.0, max.1);
+
+			let branch = HuffNode::create_branch(Box::new(left), Box::new(right));
+
+			HuffBTree { root: Some(Box::new(branch))}
+		}
+
+		fn add_leaf(&mut self, leaf: (char, usize)) {
+			let new_leaf = HuffNode::create_leaf(leaf.0, leaf.1);
+
+			if let Some(old_root) = self.root.take() {
+				let left:Box<HuffNode>;
+				let right:Box<HuffNode>;
+
+				if old_root.get_weight() < new_leaf.get_weight() {
+					left = old_root;
+					right = Box::new(new_leaf);
+				} else {
+					left = Box::new(new_leaf);
+					right = old_root;
+				}
+
+				let new_branch = HuffNode::create_branch(left, right);
+				self.root = Some(Box::new(new_branch));
+			}
+		}
+
+		fn is_lower_eq(&mut self, next: usize) -> bool {
+			self.root.as_ref().map_or(false, |root| root.get_weight() <= next)
+		}
+	}
 }
 
 pub mod freq_calc {
@@ -42,7 +119,6 @@ mod tests {
  use crate::{cli::open_file, freq_calc::calculate_frequencies};
 	
 	fn run_file() -> Result<BTreeMap<char,usize>,Box<dyn Error>> {
-		let mut freq: BTreeMap<char, usize> = BTreeMap::new();
 		let filepath = PathBuf::from_str("/Users/lucascounagobarreiro/Documents/GitHub/rustHuff/docs/lesMiserables.txt");
  	let mut reader = open_file(filepath.unwrap()).unwrap();
 
@@ -50,9 +126,7 @@ mod tests {
 
  	Read::read_to_string(&mut reader, &mut file_str).unwrap();
 
- 	freq = calculate_frequencies(file_str).unwrap();
-
-		Ok(freq)
+		Ok(calculate_frequencies(file_str).unwrap())
 	}
 
  #[test]
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,15 @@
-use std::{collections::BTreeMap, io::Read};
+use std::io::Read;
 use rust_huff::cli::{Cli, get_args, open_file};
 use rust_huff::freq_calc::calculate_frequencies;
 fn main() {
     let Cli{file} = get_args().unwrap();
-    let mut freq: BTreeMap<char,usize> = BTreeMap::new();
 
     let mut reader = open_file(file).unwrap();
-
     let mut file_str = String::new();
-
     reader.read_to_string(&mut file_str).unwrap();
-    freq = calculate_frequencies(file_str).unwrap();
 
-    let mut sorted_freq: Vec<_> = freq.into_iter().collect();
-    sorted_freq.sort_by(|a, b| b.1.cmp(&a.1));
+    let mut sorted_freq: Vec<_> = calculate_frequencies(file_str).unwrap().into_iter().collect();
+    sorted_freq.sort_by(|a, b| a.1.cmp(&b.1));
 
     println!("{:?}", sorted_freq)
 }
